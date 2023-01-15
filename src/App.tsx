ad9f16df16bb9fd16a207b0f1da024ae1522f6ec
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-01-15 19:04:15 +0900

    app.tsxにgraphql-requestとswrを使ってデータ取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
 import { request, gql } from "graphql-request";
+import useSWR from "swr";
 
+const API = "https://countries.trevorblades.com";
 const query = gql`
-  {
-    company {
-      ceo
-    }
-    roadster {
-      apoapsis_au
+  query ExampleQuery {
+    countries {
+      code
+      name
     }
   }
 `;
 
-function App() {
-  request("https://api.spacex.land/graphql/", query).then((data) =>
-    console.log(data)
-  );
+type FetchData = {
+  countries: {
+    code: string;
+    name: string;
+  }[];
+};
 
+const GetCountries = () => {
+  const { data, error } = useSWR<FetchData>(query, (query) =>
+    request(API, query)
+  );
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  return data.countries.map((country) => (
+    <li key={country.code}>{country.name}</li>
+  ));
+};
+
+function App() {
   return (
     <div>
       <header className="p-3 h-10 bg-slate-300">test</header>
-      <main className="p-3">main</main>
+      <main className="p-3">{GetCountries()}</main>
     </div>
   );
 }
