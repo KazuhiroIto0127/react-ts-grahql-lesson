2dcd178beafda058b040e6ca49a9b91fd50c6e99
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-01-15 18:43:16 +0900

    app.tsxを変更

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import './App.css';
 function App() {
   return (
     <div>
-      <header>
+      <header className='p-3 h-10 bg-slate-300'>
         test
       </header>
+      <main className='p-3'>
+        main
+      </main>
     </div>
   );
 }
