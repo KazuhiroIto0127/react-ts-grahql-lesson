8c1bb6472eed7fc5ee539c2ca29e9289c774a989
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-01-15 18:52:57 +0900

    gitignoreに.vscode追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import './App.css';
+import { request, gql } from "graphql-request";
+
+const query = gql`
+  {
+    company {
+      ceo
+    }
+    roadster {
+      apoapsis_au
+    }
+  }
+`;
 
 function App() {
+  request("https://api.spacex.land/graphql/", query).then((data) =>
+    console.log(data)
+  );
+
   return (
     <div>
-      <header className='p-3 h-10 bg-slate-300'>
-        test
-      </header>
-      <main className='p-3'>
-        main
-      </main>
+      <header className="p-3 h-10 bg-slate-300">test</header>
+      <main className="p-3">main</main>
     </div>
   );
 }
